2e5c3dcb7bde165dd05d671e8d36a093922e20a8
Mahbod Sabbaghi <34288711+dobham@users.noreply.github.com>
2018-11-05 10:15:18 -0500

    If you go to homepage it auto detects if you logged in

--- APPEL/homepageappel.php
+++ APPEL/homepageappel.php
@@ -1,6 +1,36 @@
 <?php
+session_start();
 include "connectappel.php";
-
+if(isset($_SESSION['id'])){
+    $userID=$_SESSION['id'];
+    $sql = "SELECT * FROM login WHERE id = '$userID'";
+    $result = $conn->query($sql);
+    if($row = $result->fetch_assoc()){
+        echo "<p>Login Succesful</p>";
+        $username=$row['username'];
+        $_SESSION['id']=$row['id'];
+        $logged_in == true;
+        header('Location: homepageappel.php');
+	}
+}elseif (isset($_POST['login']) && $_POST['username'] != null){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+    if($row = $result->fetch_assoc()){
+        echo "<p>Login Succesful</p>";
+        $_SESSION['id']=$row['id'];
+        $logged_in == true;
+        header('Location: homepageappel.php');
+    }
+    elseif($_POST["username"]==null){
+        echo "Error: username not submitted<br>";
+    }
+    else {
+        echo "<p>User not found</p>";
+        $logged_in = false;
+    }
+}
 ?>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" style="display: inline;">
 <input type="submit" name="home" class="buttons" value="Appel">
@@ -19,3 +49,4 @@ include "connectappel.php";
 </fieldset>
 <label for="announce">Announcement</label><br><textarea name="announce" form="main_form" id="announce"></textarea>
 </form>
+
