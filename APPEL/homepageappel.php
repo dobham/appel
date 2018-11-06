763acfd8e080627e7c82a3c47011befb2fd073e7
Mahbod Sabbaghi <34288711+dobham@users.noreply.github.com>
2018-11-06 09:28:16 -0500

    Better Annoucements

--- APPEL/homepageappel.php
+++ APPEL/homepageappel.php
@@ -10,8 +10,7 @@ if(isset($_SESSION['id'])){
         echo "<p>Login Succesful</p>";
         $username=$row['username'];
         $_SESSION['id']=$row['id'];
-        $logged_in == true;
-        header('Location: homepageappel.php');
+        $logged_in = true;
 	}
 }elseif (isset($_POST['login']) && $_POST['username'] != null){
     $username = $_POST['username'];
@@ -21,8 +20,7 @@ if(isset($_SESSION['id'])){
     if($row = $result->fetch_assoc()){
         echo "<p>Login Succesful</p>";
         $_SESSION['id']=$row['id'];
-        $logged_in == true;
-        header('Location: homepageappel.php');
+        $logged_in = true;
     }
     elseif($_POST["username"]==null){
         echo "Error: username not submitted<br>";
@@ -33,6 +31,16 @@ if(isset($_SESSION['id'])){
     }
 }
 if ($logged_in){
+	if(isset($_POST['announce'])){
+		$announcement=$_POST['announcement'];
+		$organ=$_POST['organ'];
+		$sponsors=$_POST['sponsors'];
+		$sql="INSERT INTO announcement (announcement, organization, sponsor) VALUES ('$announcement', '$organ', '$sponsors')";
+		if($conn->query($sql)){
+		}else{
+			echo "ERROR: ", $conn->error;
+		}
+	}
 ?>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" style="display: inline;">
 <input type="submit" name="home" class="buttons" value="Appel">
@@ -44,13 +52,24 @@ if ($logged_in){
 <br>
 <form action="<?php echo htmlspecialchars($_SERVER['$PHP_SELF']); ?>" method="post" id="main_form">
 <fieldset> 
-	<label for="date">Date: </label> <input type="date" id="date">
-	<label for="organ">Name of organization: </label> <input type="text" id="organ">
-	<label for="sponsors">Staff sponsors</label> <input type="text" id="sponsors>
-	<input type="submit">
+	<label for="dateStart">Date start: </label> <input type="date" id="dateStart" name="dateStart">
+	<label for="dateEnd">Date end: </label> <input type="date" id="dateEnd" name=dateEnd>
+	<label for="organ">Name of organization: </label> <input type="text" id="organ" name="organ">
+	<label for="sponsors">Staff sponsors</label> <input type="text" id="sponsors" name="sponsors">	
+	<div>
+		<label for="monday" >Monday</label><input type="checkbox" id="monday" name=monday><br>
+		<label for="tuesday">Tuesday</label><input type="checkbox" id="tuesday" name=tuesday><br>
+		<label for="wednesday">Wednesday</label><input type="checkbox" id="wednesday" name=wednesday><br>
+		<label for="thursday">Thursday</label><input type="checkbox" id="thursday" name=thursday><br>
+		<label for="friday">Friday</label><input type="checkbox" id="friday" name=friday><br>
+		<label for="saturday">Saturday</label><input type="checkbox" id="saturday" name=saturday><br>
+		<label for="sunday">Sunday</label><input type="checkbox" id="sunday" name=sunday>
+	</div>
+	<input type="submit" name="announce">
 </fieldset>
-<label for="announce">Announcement</label><br><textarea name="announce" form="main_form" id="announce"></textarea>
+<label for="announce">Announcement</label><br><textarea name="announcement" form="main_form" id="announce"></textarea>
 </form>
 <?php }else{
-	header("Location: welcomeappel.php");
+echo "not logged";
+	//header("Location: welcomeappel.php");
 } ?>
