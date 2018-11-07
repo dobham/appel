e106bc7c61cbfe1290aa0edfff366c01c19b7485
antimonyB <anthony.bertnyk@gmail.com>
2018-11-06 22:08:24 -0500

    Added announcement display.
    Homepage now shows announcements for current day.

--- APPEL/homepageappel.php
+++ APPEL/homepageappel.php
@@ -2,6 +2,7 @@
 session_start();
 $logged_in = false;
 include "connectappel.php";
+date_default_timezone_set("America/Toronto");
 if(isset($_SESSION['id'])){
     $userID=$_SESSION['id'];
     $sql = "SELECT * FROM login WHERE id = '$userID'";
@@ -37,14 +38,25 @@ if ($logged_in){
 		$sponsors=$_POST['sponsors'];
 		$startDate=$_POST['startDate'];
 		$endDate=$_POST['endDate'];
-		$weekDays=$_POST['weekDays'];
-		$weekDaysStr=implode(",",$weekDays);
-		$sql="INSERT INTO announcement (announcement, organization, sponsors, startDate, endDate, weekDays) VALUES ('$announcement', '$organ', '$sponsors', '$startDate', '$endDate', '$weekDaysStr')";
+		$weekdays=$_POST['weekdays'];
+		$weekdaysStr=implode(",",$weekdays);
+		$sql="INSERT INTO announcement (announcement, organization, sponsors, startDate, endDate, weekdays) VALUES ('$announcement', '$organ', '$sponsors', '$startDate', '$endDate', '$weekdaysStr')";
 		if($conn->query($sql)){
 			echo "Announcement submitted";
 		}else{
 			echo "ERROR: ", $conn->error;
 		}
+	}
+	$currentDate=date('Y-m-d');
+	$currentWeekday=date('D');
+	$sql="SELECT * FROM announcement WHERE startDate <= CONVERT('$currentDate', DATE) AND endDate >= CONVERT('$currentDate', DATE) AND weekdays LIKE '%$currentWeekday%'";
+	$result=$conn->query($sql);
+	if($result=$conn->query($sql)){
+	}else{
+		echo "ERROR: " . $sql . " - " . $conn->error;
+	}
+	while($row=$result->fetch_assoc()){
+		echo "<br><b>" . $row['organization'] . " - " . $row['sponsors'] . "</b><br>" . $row['announcement'] . "<br>";
 	}
 ?>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" style="display: inline;">
@@ -57,16 +69,16 @@ if ($logged_in){
 <br>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" id="main_form">
 <fieldset>
-	<label for="dateStart">Date start: </label> <input type="date" id="dateStart" name="endDate">
-	<label for="dateEnd">Date end: </label> <input type="date" id="dateEnd" name="startDate">
+	<label for="dateStart">Date start: </label> <input type="date" id="dateStart" name="startDate">
+	<label for="dateEnd">Date end: </label> <input type="date" id="dateEnd" name="endDate">
 	<label for="organ">Name of organization: </label> <input type="text" id="organ" name="organ">
 	<label for="sponsors">Staff sponsors</label> <input type="text" id="sponsors" name="sponsors">	
 	<div>
-		<label for="monday" >Monday</label><input type="checkbox" id="monday" name="weekDays[]" value=monday><br>
-		<label for="tuesday">Tuesday</label><input type="checkbox" id="tuesday" name="weekDays[]" value=tuesday><br>
-		<label for="wednesday">Wednesday</label><input type="checkbox" id="wednesday" name="weekDays[]" value=wednesday><br>
-		<label for="thursday">Thursday</label><input type="checkbox" id="thursday" name="weekDays[]" value=thursday><br>
-		<label for="friday">Friday</label><input type="checkbox" id="friday" name="weekDays[]" value=friday><br>
+		<label for="monday" >Monday</label><input type="checkbox" id="monday" name="weekdays[]" value=monday><br>
+		<label for="tuesday">Tuesday</label><input type="checkbox" id="tuesday" name="weekdays[]" value=tuesday><br>
+		<label for="wednesday">Wednesday</label><input type="checkbox" id="wednesday" name="weekdays[]" value=wednesday><br>
+		<label for="thursday">Thursday</label><input type="checkbox" id="thursday" name="weekdays[]" value=thursday><br>
+		<label for="friday">Friday</label><input type="checkbox" id="friday" name="weekdays[]" value=friday><br>
 	</div>
 	<input type="submit" name="announce" value="Submit">
 </fieldset>
