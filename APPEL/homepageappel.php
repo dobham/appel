268ef381cdd4f5ea1c79a9f5c68ec7cd90bc2913
Mahbod Sabbaghi <34288711+dobham@users.noreply.github.com>
2018-11-08 09:54:11 -0500

    Organized display and added delete

--- APPEL/homepageappel.php
+++ APPEL/homepageappel.php
@@ -37,34 +37,14 @@ if(isset($_SESSION['id'])){
         $logged_in = false;
     }
 }
-if ($logged_in){
-        echo "<p>Welcome $access $username</p>";
-	if(isset($_POST['announce'])){
-		$announcement=$_POST['announcement'];
-		$organ=$_POST['organ'];
-		$sponsors=$_POST['sponsors'];
-		$startDate=$_POST['startDate'];
-		$endDate=$_POST['endDate'];
-		$weekdays=$_POST['weekdays'];
-		$weekdaysStr=implode(",",$weekdays);
-		$sql="INSERT INTO announcement (announcement, organization, sponsors, userid, startDate, endDate, weekdays) VALUES ('$announcement', '$organ', '$sponsors', '$userID', '$startDate', '$endDate', '$weekdaysStr')";
-		if($conn->query($sql)){
-			echo "Announcement submitted";
-		}else{
-			echo "ERROR: ", $conn->error;
-		}
-	}
+if ($logged_in){ //LOGGED IN STARTS HERE
 	$currentDate=date('Y-m-d');
 	$currentWeekday=date('D');
-	$sql="SELECT * FROM announcement WHERE startDate <= CONVERT('$currentDate', DATE) AND endDate >= CONVERT('$currentDate', DATE) AND weekdays LIKE '%$currentWeekday%'";
-	$result=$conn->query($sql);
-	if($result=$conn->query($sql)){
-	}else{
-		echo "ERROR: " . $sql . " - " . $conn->error;
+	if(isset($_POST['submitDisplayDate'])){
+		$currentDate=$_POST['displayDate'];
+		$currentWeekday=date('D', strtotime($currentDate));
 	}
-	while($row=$result->fetch_assoc()){
-		echo "<br><b>" . $row['organization'] . " - " . $row['sponsors'] . "</b><br>" . $row['announcement'] . "<br>";
-	}
+        echo "<p>Welcome $access $username</p>";
 ?>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" style="display: inline;">
 <input type="submit" name="home" class="buttons" value="Appel">
@@ -74,6 +54,14 @@ if ($logged_in){
 </form>
 <br>
 <br>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <input type="submit" name="createAnnounce" class="buttons" value="Create Announcement">
+        <input type="submit" name="showAll" class="buttons" value="Show all announcements">
+        <input type="submit" name="showMy" class="buttons" value="Show my announcements">
+</form>
+<?php
+	if(isset($_POST['createAnnounce'])){
+?>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" id="main_form">
 <fieldset>
 	<label for="dateStart">Date start: </label> <input type="date" id="dateStart" name="startDate">
@@ -92,6 +80,74 @@ if ($logged_in){
 </fieldset>
 <label for="announce">Announcement</label><br><textarea name="announcement" form="main_form" id="announce"></textarea>
 </form>
-<?php }else{
+<?php
+	}elseif(isset($_POST['showAll'])){
+		//echo "Showing announcements for  $currentDate $currentWeekday";
+?>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" id="main_form">
+	<label>Filter date</label> <input type="date" id="dateStart" name="displayDate" value="<?php echo $currentDate; ?>">
+	<input type="hidden" id="dateStart" name="showAll">
+	<input type="submit" id="submit" name="submitDisplayDate" value="Go">
+</form>
+<?php
+		echo "<br>announcements here.";
+		$sql="SELECT * FROM announcement WHERE startDate <= CONVERT('$currentDate', DATE) AND endDate >= CONVERT('$currentDate', DATE) AND weekdays LIKE '%$currentWeekday%'";
+		$result=$conn->query($sql);
+		if($result=$conn->query($sql)){
+		}else{
+			echo "ERROR: " . $sql . " - " . $conn->error;
+		}
+		while($row=$result->fetch_assoc()){
+			echo "<br><b>" . $row['organization'] . " - " . $row['sponsors'] . "</b> "
+?>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" id="main_form" style="display: inline;">
+	<input type="hidden" id="dateStart" name="deletion" value="<?php echo $row['id'] ?>">
+	<input type="submit" id="delete" name="delete" value="Delete">
+</form>
+<?php
+			echo "<br>" . $row['announcement'] . "<br>";
+		}
+	}elseif(isset($_POST['showMy'])){
+		echo "announcements here.";
+		$sql="SELECT * FROM announcement WHERE userid='$userID'";
+		$result=$conn->query($sql);
+		if($result=$conn->query($sql)){
+		}else{
+			echo "ERROR: " . $sql . " - " . $conn->error;
+		}
+		while($row=$result->fetch_assoc()){
+			echo "<br><b>" . $row['organization'] . " - " . $row['sponsors'] . "</b><br>" . $row['announcement'] . "<br>";
+		}
+	}
+	if(isset($_POST['announce'])){
+		$announcement=$_POST['announcement'];
+		$organ=$_POST['organ'];
+		$sponsors=$_POST['sponsors'];
+		$startDate=$_POST['startDate'];
+		$endDate=$_POST['endDate'];
+		$weekdays=$_POST['weekdays'];
+		$weekdaysStr=implode(",",$weekdays);
+		$sql="INSERT INTO announcement (announcement, organization, sponsors, userid, startDate, endDate, weekdays) VALUES ('$announcement', '$organ', '$sponsors', '$userID', '$startDate', '$endDate', '$weekdaysStr')";
+		if($conn->query($sql)){
+			echo "Announcement submitted";
+		}else{
+			echo "ERROR: ", $conn->error;
+		}
+	}
+	if(isset($_POST['delete'])){
+		var_dump($_POST['deletion']);
+		$deletion=$_POST['deletion'];
+		echo $deletion;
+		$sql="DELETE FROM announcement WHERE id='$deletion'";
+		if($conn->query($sql) === TRUE){
+			echo "Deleted successfully";
+		}else{
+			echo "ERROR with deletion: " . $conn->error;
+		}
+	}
+?>
+
+<?php
+}else{
 	header("Location: welcomeappel.php");
 } ?>
